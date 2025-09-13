96caf574efc090481b3edf77aa1c15c0ebb4fd29
Anoopastro <singhamlady9@gmail.com>
2025-09-13 22:49:41 +0530

    Update ephemeris_provider.dart

--- lib/providers/ephemeris_provider.dart
+++ lib/providers/ephemeris_provider.dart
@@ -1,47 +1,74 @@
+import 'dart:convert';
 import 'package:flutter/services.dart';
+import 'package:flutter/foundation.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:sweph/sweph.dart';
+import 'package:intl/intl.dart';
 
-class EphemerisProvider {
+class EphemerisProvider extends ChangeNotifier {
   final Sweph _swe = Sweph();
+  Map<String, double> planets = {};
+  List<double> houses = [];
+  late String city;
+  late DateTime birthDate;
 
-  EphemerisProvider();
+  Future<void> initialize() async {
+    // Load local ephemeris file
+    final ephemData = await rootBundle.load('assets/ephem/ephem_1950_2050.dat');
+    _swe.loadBinaryEphemeris(ephemData.buffer.asUint8List());
 
-  Future<void> loadFromAsset(String assetPath) async {
-    final data = await rootBundle.load(assetPath);
-    _swe.loadBinaryEphemeris(data.buffer.asUint8List());
+    // Get current location
+    final pos = await Geolocator.getCurrentPosition(desiredAccuracy: LocationAccuracy.high);
+    city = await _getNearestCity(pos.latitude, pos.longitude);
+
+    birthDate = DateTime.now(); // Default now, can be user input
+
+    await _calculatePlanets(pos.latitude, pos.longitude);
+    await _calculateHouses(pos.latitude, pos.longitude);
   }
 
-  Map<String, double> calculatePlanets(double jd) {
-    final Map<String, double> planets = {};
-    final Map<int, String> planetMap = {
-      SweConst.SE_SUN: "Sun",
-      SweConst.SE_MOON: "Moon",
-      SweConst.SE_MERCURY: "Mercury",
-      SweConst.SE_VENUS: "Venus",
-      SweConst.SE_MARS: "Mars",
-      SweConst.SE_JUPITER: "Jupiter",
-      SweConst.SE_SATURN: "Saturn",
-      SweConst.SE_URANUS: "Uranus",
-      SweConst.SE_NEPTUNE: "Neptune",
-      SweConst.SE_PLUTO: "Pluto",
-      SweConst.SE_TRUE_NODE: "Rahu",
-      SweConst.SE_MEAN_NODE: "Ketu",
+  Future<String> _getNearestCity(double lat, double lon) async {
+    final data = await rootBundle.loadString('assets/cities/cities.json');
+    final List cities = jsonDecode(data);
+    // Simple nearest city calculation (can enhance)
+    cities.sort((a, b) {
+      final distA = (a['lat'] - lat).abs() + (a['lon'] - lon).abs();
+      final distB = (b['lat'] - lat).abs() + (b['lon'] - lon).abs();
+      return distA.compareTo(distB);
+    });
+    return cities.first['name'];
+  }
+
+  Future<void> _calculatePlanets(double lat, double lon) async {
+    final jd = _swe.swe_julday(
+        birthDate.year, birthDate.month, birthDate.day, birthDate.hour + birthDate.minute / 60, Sweph.SE_GREG_CAL);
+    final Map<int, String> planetNames = {
+      SweConst.SE_SUN: 'Sun',
+      SweConst.SE_MOON: 'Moon',
+      SweConst.SE_MERCURY: 'Mercury',
+      SweConst.SE_VENUS: 'Venus',
+      SweConst.SE_MARS: 'Mars',
+      SweConst.SE_JUPITER: 'Jupiter',
+      SweConst.SE_SATURN: 'Saturn',
+      SweConst.SE_URANUS: 'Uranus',
+      SweConst.SE_NEPTUNE: 'Neptune',
+      SweConst.SE_PLUTO: 'Pluto',
+      SweConst.SE_TRUE_NODE: 'Rahu',
+      SweConst.SE_MEAN_NODE: 'Ketu',
     };
 
-    planetMap.forEach((key, name) {
+    planets.clear();
+    for (var key in planetNames.keys) {
       final pos = _swe.calc(jd, key, SweConst.SEFLG_SWIEPH);
-      planets[name] = pos[0]; // longitude
-    });
-
-    return planets;
+      planets[planetNames[key]!] = pos[0]; // Longitude
+    }
+    notifyListeners();
   }
 
-  Map<String, double> calculateHouses(double jd, double lat, double lon) {
-    final cusps = _swe.houses(jd, lat, lon, 'P'); // Placidus
-    final Map<String, double> houses = {};
-    for (int i = 1; i <= 12; i++) {
-      houses['House $i'] = cusps[i - 1];
-    }
-    return houses;
+  Future<void> _calculateHouses(double lat, double lon) async {
+    final jd = _swe.swe_julday(
+        birthDate.year, birthDate.month, birthDate.day, birthDate.hour + birthDate.minute / 60, Sweph.SE_GREG_CAL);
+    houses = _swe.houses(jd, lat, lon, 'P'); // Placidus
+    notifyListeners();
   }
 }
