55dd3b0102292ffcd8cc87e1ba1f3fcb651a7f58
Anoopastro <singhamlady9@gmail.com>
2025-09-21 06:51:05 +0530

    Delete test directory

--- test/widget_test.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter_test/flutter_test.dart';
-import 'package:astro_app/main.dart';
-
-void main() {
-  testWidgets('App has title', (WidgetTester tester) async {
-    await tester.pumpWidget(const MyApp());
-    expect(find.text('Astro App'), findsOneWidget);
-  });
-}
